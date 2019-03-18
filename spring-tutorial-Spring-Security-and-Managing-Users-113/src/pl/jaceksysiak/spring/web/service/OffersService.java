d20e91e9d7cce0fd3171225444a282d1c8729e77
j4sysiak <j4sysiak@gmail.com>
2019-03-18 18:22:11 +0100

    1

---
+++ spring-tutorial-Spring-Security-and-Managing-Users-113/src/pl/jaceksysiak/spring/web/service/OffersService.java
@@ -0,0 +1,71 @@
+package pl.jaceksysiak.spring.web.service;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import pl.jaceksysiak.spring.web.dao.Offer;
+import pl.jaceksysiak.spring.web.dao.OffersDao;
+
+@Service("offersService")
+public class OffersService {
+	
+	private OffersDao offersDao;
+	
+	@Autowired
+	public void setOffersDao(OffersDao offersDao) {
+		this.offersDao = offersDao;
+	}
+
+	public List<Offer> getCurrent() {
+		return offersDao.getOffers();
+	}
+
+	@Secured({"ROLE_USER", "ROLE_ADMIN"})
+	public void create(@Valid Offer offer) {
+		offersDao.create(offer);
+	}
+
+//	public void throwTestException() {
+//		offersDao.getOffer(9999);
+//	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
